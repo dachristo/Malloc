4779f039487b48e0a1019783a4b2040601e11e57
xeiis <seis02@hotmail.fr>
2017-08-15 17:00:13 +0200

    Ajout couleur dans show_alloc_mem()

--- src/show_alloc_mem.c
+++ src/show_alloc_mem.c
@@ -6,7 +6,7 @@
 /*   By: dchristo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/23 13:49:53 by dchristo          #+#    #+#             */
-/*   Updated: 2017/08/15 16:51:58 by dchristo         ###   ########.fr       */
+/*   Updated: 2017/08/15 16:59:41 by dchristo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 static void			ft_putdata(t_region_d *data)
 {
+	ft_putstr(COLOR_YELLOW);
 	ft_putstr("0x");
 	ft_puthex((size_t)data->data);
 	ft_putstr(" - ");
@@ -23,14 +24,18 @@ static void			ft_putdata(t_region_d *data)
 	ft_putnbr(data->len);
 	ft_putstr(" octects ");
 	ft_putstr("\n");
+	ft_putstr(COLOR_RESET);
 }
 
 static void			show_tiny_mem(t_region_d *data)
 {
+	ft_putstr(ANSI_COLOR_CYAN);
+	ft_putstr(COLOR_REV);
 	ft_putstr("TINY : ");
 	ft_putstr("0x");
 	ft_puthex((size_t)data);
 	ft_putstr("\n");
+	ft_putstr(COLOR_RESET);
 	while (data != NULL)
 	{
 		if (data->isfree != 2)
@@ -41,6 +46,8 @@ static void			show_tiny_mem(t_region_d *data)
 
 static void			show_small_mem(t_region_d *data)
 {
+	ft_putstr(ANSI_COLOR_BLUE);
+	ft_putstr(COLOR_REV);
 	ft_putstr("SMALL : ");
 	ft_putstr("0x");
 	ft_puthex((size_t)data);
@@ -55,6 +62,8 @@ static void			show_small_mem(t_region_d *data)
 
 static void			show_large_mem(t_region_d *data, t_alloc *alloc)
 {
+	ft_putstr(ANSI_COLOR_MAGENTA);
+	ft_putstr(COLOR_REV);
 	ft_putstr("LARGE : ");
 	ft_putstr("0x");
 	ft_puthex((size_t)data);
@@ -64,11 +73,13 @@ static void			show_large_mem(t_region_d *data, t_alloc *alloc)
 		ft_putdata(data);
 		data = data->next;
 	}
+	ft_putstr(COLOR_RED);
 	ft_putstr("Total : ");
 	ft_putnbr((SMALL * alloc->total_s_used + alloc->size_s_used) +
 			(TINY * alloc->total_t_used + alloc->size_t_used) +
 			(alloc->size_l_used));
 	ft_putstr(" octects\n");
+	ft_putstr(COLOR_RESET);
 }
 
 void				show_alloc_mem(void)
